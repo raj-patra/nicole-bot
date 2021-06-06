0875edd45c74de469f13ea4debb5ad88b63cf46c
Raj Kishore Patra <rajpatra.kishore@gmail.com>
2021-06-06 20:13:40 +0530

    Added AIML part

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
-import logging, os
+import logging, os, aiml
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+TOKEN = str(os.environ['BOT_TOKEN'])
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-TOKEN = str(os.environ['BOT_TOKEN'])
+# Initialise AIML Kernel
+kernel = aiml.Kernel()
+kernel.setBotPredicate("name", "Nicole")
+
+# Load/Learn Brain file
+if os.path.isfile("bot_brain.brn"):
+    kernel.bootstrap(brainFile="bot_brain.brn")
+else:
+    kernel.bootstrap(learnFiles="startup.xml", commands="LOAD AIML B")
+    kernel.saveBrain("bot_brain.brn")
 
 def start(update, context):
     update.message.reply_text("Hi! I am Nicole, a conversational chatbot. GLHF")
@@ -13,8 +24,8 @@ def start(update, context):
 def help(update, context):
     update.message.reply_text("Made with Py3 and AIML. For any queries contact https://t.me/a_ignorant_mortal")
 
-def echo(update, context):
-    update.message.reply_text(update.message.text)
+def respond(update, context):
+    update.message.reply_text(kernel.respond(update.message.text))
 
 def error(update, context):
     logger.warning('Update "%s" caused error "%s"', update, context.error)
@@ -35,7 +46,7 @@ def main():
     dp.add_handler(CommandHandler("help", help))
 
     # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(MessageHandler(Filters.text, echo))
+    dp.add_handler(MessageHandler(Filters.text, respond))
 
     # log all errors
     dp.add_error_handler(error)
