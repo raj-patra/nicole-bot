179443d6274cfbd39c0b17f1c4c03d44662cde96
Raj Kishore Patra <rajpatra.kishore@gmail.com>
2021-06-06 19:07:27 +0530

    Webhooks instead of Polling

--- app.py
+++ app.py
@@ -42,9 +42,9 @@ def main():
     dp.add_error_handler(error)
 
     # Start the Bot
-    updater.start_polling()
+    # updater.start_polling()
     # updater.start_webhook(listen='0.0.0.0', port=int(PORT), url_path=TOKEN)
-    # updater.bot.setWebhook('https://nicole-bot.herokuapp.com/' + TOKEN)
+    updater.bot.setWebhook('https://nicole-bot.herokuapp.com/' + TOKEN)
 
     updater.idle()
 
