d867a3c948d497eadbe489a9f695e811bf6a4ef2
Raj Kishore Patra <rajpatra.kishore@gmail.com>
2021-07-03 21:09:51 +0530

    Added new useful websites

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
-# from config import *
 from bot import NicoleBot
 
 import os
--- config.py
+++ config.py
@@ -1,13 +1,18 @@
+# ----------------------------------------------------------------------------------
+
 DOG_PIC_URL = "https://dog.ceo/api/breeds/image/random"
 DOG_CAP_URL = "https://dog-facts-api.herokuapp.com/api/v1/resources/dogs?number=1"
 
 CAT_PIC_URL = "https://thatcopy.pw/catapi/rest/"
 CAT_CAP_URL = "https://cat-fact.herokuapp.com/facts/random"
 
-RANDOM_HUMAN_URL = "https://thispersondoesnotexist.com/image"
+# ----------------------------------------------------------------------------------
 
+RANDOM_HUMAN_URL = "https://thispersondoesnotexist.com/image"
 MEME_URL = "https://meme-api.herokuapp.com/gimme"
 NAMO_URL = "https://namo-memes.herokuapp.com/memes/1"
+
+# ----------------------------------------------------------------------------------
 
 QUOTE_URL = "https://api.quotable.io/random"
 INSULT_URL = "https://evilinsult.com/generate_insult.php?lang=en&type=json"
@@ -17,19 +22,30 @@ KANYE_URL = "https://api.kanye.rest/"
 TRUMP_URL = "https://api.whatdoestrumpthink.com/api/v1/quotes/random"
 DAILY_URL = "https://www.boredapi.com/api/activity/"
 
-RANDOM_WEBSITE_URL = "https://www.boredbutton.com/random"
+# ----------------------------------------------------------------------------------
 
+RANDOM_WEBSITE_URL = "https://www.boredbutton.com/random"
 AGE_URL = "https://api.agify.io/"
+
+# ----------------------------------------------------------------------------------
 
 USEFUL_WEBSITE_URL = """
 *Services*
 Assemble all your links - [LinkTree](https://linktr.ee/)
 Disposable temporary email - [Temp Mail](https://temp-mail.org/en/)
 Internet spped test - [Pure Speed](http://openspeedtest.com/?ref=OST-Results)
+Read an interesting article - [Read Something Great](https://www.readsomethinggreat.com/)
 
 *Utilities*
 Free web apps - [123apps](https://123apps.com/)
 Extract mp3 from youtube videos - [Youtube to MP3](https://y2mate.guru/en4/youtube-to-mp3/)
 Convert any file online - [Cloud Convert](https://cloudconvert.com/)
 Remove background from image - [Remove.bg](https://www.remove.bg/upload)
+
+*Fun*
+Create and share memes - [Imgflip.com](https://imgflip.com/)
+Random websites - [Bored Button](https://www.boredbutton.com/random)
+Live wallpaper - [Arkadia](http://arkadia.xyz/)
+If the moon were only 1 pixel - [Solar Syatem Map](https://joshworth.com/dev/pixelspace/pixelspace_solarsystem.html)
+Spotify Visualizer - [Wavesync](https://wavesync.herokuapp.com/visualizer)
 """
