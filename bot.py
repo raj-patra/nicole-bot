df1ba3083bf219fc71ca9f224f7f2cd8364da10a
Raj Kishore Patra <rajpatra.kishore@gmail.com>
2021-07-05 22:15:14 +0530

    Minor fixes

--- bot.py
+++ bot.py
@@ -35,9 +35,9 @@ class NicoleBot:
                         ]
         self.text_menu =[
                             [tg.InlineKeyboardButton('Quote of the Day 💯', callback_data='txt_quote'), tg.InlineKeyboardButton('Fact of the Day 🤯', callback_data='txt_facts')],
-                            [tg.InlineKeyboardButton('Daily Activity 🎬', callback_data='txt_daily')],
-                            [tg.InlineKeyboardButton('Kanye REST 🧭', callback_data='txt_kanye'), tg.InlineKeyboardButton('Donald Grump 🎺', callback_data='txt_trump')], 
                             [tg.InlineKeyboardButton('A Literati\'s Wet Dream 🎶', callback_data='txt_poems')],
+                            [tg.InlineKeyboardButton('Kanye REST 🧭', callback_data='txt_kanye'), tg.InlineKeyboardButton('Donald Grump 🎺', callback_data='txt_trump')], 
+                            [tg.InlineKeyboardButton('Daily Activity 🎬', callback_data='txt_daily')],
                             [tg.InlineKeyboardButton('◀ Back', callback_data='main_back'), tg.InlineKeyboardButton('Cancel Op ❌', callback_data='main_cancel')]
                         ]
         self.tool_menu =[
