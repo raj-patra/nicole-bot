698096ce07d5c050415802e5cf9ca9abe5975a7a
Raj Kishore Patra <rajpatra.kishore@gmail.com>
2021-10-25 09:36:26 +0530

    Minor fix in api

--- helpers/api.py
+++ helpers/api.py
@@ -148,23 +148,23 @@ def get_caption(query_data):
             response = requests.get(urls.QUOTE_API).json()
             caption = "*{}* \n\n- {}".format(response['content'], response['author'])
 
-        if query_data == 'txt_facts':
+        elif query_data == 'txt_facts':
             response = requests.get(urls.FACTS_API).json()
             caption = "Did you know, \n\n*{}*".format(response['text'])
 
-        if query_data == 'txt_poems':
+        elif query_data == 'txt_poems':
             response = random.choice(requests.get(urls.POEMS_API).json())
             caption = "*{}* \n\n{} \n\nBy *{}*".format(response['title'], response['content'], response['poet']['name'])
 
-        if query_data == 'txt_kanye':
+        elif query_data == 'txt_kanye':
             response = requests.get(urls.KANYE_API).json()
             caption = "Kanye REST once said, \n\n*{}*".format(response['quote'])
 
-        if query_data == 'txt_trump':
+        elif query_data == 'txt_trump':
             response = requests.get(urls.TRUMP_API).json()
             caption = "Grumpy Donald once said, \n\n*{}*".format(response['message'])
 
-        if query_data == 'txt_trivia':
+        elif query_data == 'txt_trivia':
             response = requests.get(random.choice(urls.TRIVIA_API)).text
             caption = "Here's a number trivia that you will probably never need.  \n\n*{}*".format(response)
 
