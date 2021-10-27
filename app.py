ee42a148ef4606b414be2e5e6ce9a1d377463ce3
Raj Kishore Patra <rajpatra.kishore@gmail.com>
2021-10-27 11:12:34 +0530

    Added anime quotes to text menu

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ import re, os
 PORT = os.environ.get('PORT', 3000)
 AUTH = os.environ.get('NICOLE_BOT_TOKEN')
 HOOK = os.environ.get('WEBHOOK')
-HOOK = "https://3edb-103-199-182-164.ngrok.io"
 
 def app():
     bot = NicoleBot()
