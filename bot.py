3ebac9f2be3b867bfd72929486b08a78e6d17666
Raj Kishore Patra <rajpatra.kishore@gmail.com>
2021-10-30 13:21:56 +0530

    Updated keyboard

--- bot.py
+++ bot.py
@@ -28,8 +28,8 @@ class NicoleBot:
         self.logger = logging.getLogger(__name__)
 
         self.main_menu =tg.InlineKeyboardMarkup([
-                            [tg.InlineKeyboardButton('Image Services 🌆', callback_data="main_image"), tg.InlineKeyboardButton('Utilities 🛠', callback_data="main_tools")],
-                            [tg.InlineKeyboardButton('Quotify 📝', callback_data="main_text"), tg.InlineKeyboardButton('Recreation 🥳', callback_data="main_fun")],
+                            [tg.InlineKeyboardButton('Visuals 🌆', callback_data="main_image"), tg.InlineKeyboardButton('Quotify 📝', callback_data="main_text")],
+                            [tg.InlineKeyboardButton('Recreation 🥳', callback_data="main_fun"), tg.InlineKeyboardButton('Utilities 🛠', callback_data="main_tools")],
                             [tg.InlineKeyboardButton('Cancel Op ❌', callback_data='main_cancel')]
                         ])
         self.image_menu=tg.InlineKeyboardMarkup([
