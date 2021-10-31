39b17d5736766994455a9817e5bf4042d0edb206
Raj Kishore Patra <rajpatra.kishore@gmail.com>
2021-10-31 11:36:23 +0530

    Minor refactor in error handling

--- bot.py
+++ bot.py
@@ -231,5 +231,5 @@ class NicoleBot:
 
     def error(self, update, context):
         self.logger.warning('Update that caused the error, \n\n"%s" \n\nThe Error "%s"', update, context.error)
-        if update.callback_query.get("id"):
+        if update.callback_query.id != None:
             context.bot.answerCallbackQuery(callback_query_id=update.callback_query.id, text=constants.ERROR_TXT, show_alert=True)
