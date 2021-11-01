0e51abc10bd633876c058ecac82b4bd1be07c5fc
Raj Kishore Patra <rajpatra.kishore@gmail.com>
2021-11-01 09:49:27 +0530

    Added new trivia apis

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ import re, os
 PORT = os.environ.get('PORT', 3000)
 AUTH = os.environ.get('NICOLE_BOT_TOKEN')
 HOOK = os.environ.get('WEBHOOK')
-HOOK = "https://276c-103-199-182-31.ngrok.io"
 
 def app():
     bot = NicoleBot()
