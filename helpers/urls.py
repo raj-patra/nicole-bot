fe0762e724be647e2804b62d26146e16c1d68405
Raj Kishore Patra <rajpatra.kishore@gmail.com>
2021-11-05 10:46:38 +0530

    Added Affirmation URL to list

--- helpers/urls.py
+++ helpers/urls.py
@@ -24,6 +24,7 @@ STOIC_API = "https://api.themotivate365.com/stoic-quote"
 ANIME_API = "https://animechan.vercel.app/api/random"
 HEROS_API = "https://superhero-quotes.herokuapp.com/random"
 ADVICE_API = "https://api.adviceslip.com/advice"
+AFFIRMATION_API = "https://www.affirmations.dev"
 
 FACTS_API = "https://uselessfacts.jsph.pl//random.json?language=en"
 POEMS_API = "https://www.poemist.com/api/v1/randompoems"
