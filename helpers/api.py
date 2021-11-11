53af34e3c9618225950bc645730a940941929006
Raj Kishore Patra <rajpatra.kishore@gmail.com>
2021-11-11 10:17:48 +0530

    Updated text api

--- helpers/api.py
+++ helpers/api.py
@@ -159,14 +159,14 @@ def get_caption(query_data):
         elif query_data == 'txt_advice':
             response = requests.get(urls.ADVICE_API).json()
             caption = "Here's my two cents, \n\n*{}*".format(response['slip']['advice'])
-
-        elif query_data == 'txt_heros':
-            response = requests.get(urls.HEROS_API).json()
-            caption = "Banner - *{}*\n\n*{}*\n\n- {}".format(response['Banner'], response['Stuff']['data']['quote'], response['Stuff']['data']['author'])
         
         elif query_data == 'txt_affirmation':
             response = requests.get(urls.AFFIRMATION_API).json()
             caption = "Hey there... Put your chin up,\n\n*{}*".format(response['affirmation'])
+
+        elif query_data == 'txt_heros':
+            response = requests.get(urls.HEROS_API).json()
+            caption = "Banner - *{}*\n\n*{}*\n\n- {}".format(response['Banner'], response['Stuff']['data']['quote'], response['Stuff']['data']['author'])
 
         error = False
     except Exception:
